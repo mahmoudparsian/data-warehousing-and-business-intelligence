4654bf0cdf1ad649e3e11fc0e77413bedc90de82
Mahmoud Parsian <mparsian@illumina.com>
2024-04-23 17:38:03 -0700

    updated schema.sql

--- resources/ETL/ETL_Insurance_Example_with_Transformation/schema.sql
+++ resources/ETL/ETL_Insurance_Example_with_Transformation/schema.sql
@@ -1,16 +1,17 @@
 use test;
 drop table insurance_from_etl2;
-create table insurance_from_etl2 (
-index int,
-age int,
-sex text,
-bmi double,
-children int,
-smoker text,
-region text,
-charges double,
-bmi_indicator text,
-age_group text);
+create table `insurance_from_etl2` (
+`index` int,
+`age` int,
+`sex` text,
+`bmi` double,
+`children` int,
+`smoker` text,
+`region` text,
+`charges` double,
+`bmi_indicator` text,
+`age_group` text
+);
 
 select * from insurance_from_etl2;
 
